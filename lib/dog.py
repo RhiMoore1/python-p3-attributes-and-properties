de401a9c7a93c60ec753ee6669b0dc79df935816
Rhiannon Moore <rhiannonmoore10@yahoo.com>
2023-07-07 10:24:08 -0700

    updates lab

--- lib/dog.py
+++ lib/dog.py
@@ -8,26 +8,80 @@ APPROVED_BREEDS = [
     "Beagle",
     "French Bulldog",
     "Pug",
- 
     "Pointer"
 ]
 
 # ##################################################################
 class Dog:
-    def __init__(self, name="Any", breed="Any"):
+    def __init__(self, name='Any', breed='Pug'):
+        self.name = name
+        self.breed = breed
 
-        if isinstance(name, str) and 0 < len(name) <= 25 and len(name) != 0:
-            self.name = name
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            self._name = name.title()
         else:
             print("Name must be string between 1 and 25 characters.")
+       
+    name = property(get_name, set_name)
 
-        
+    def get_breed(self):
+        return self._breed
+    
+    def set_breed(self, breed):
         if breed in APPROVED_BREEDS:
-            self.breed = breed
+            self._breed = breed
         else:
             print("Breed must be in list of approved breeds.")
 
-fido = Dog("Fluffy", "Pug")
+    breed = property(get_breed, set_breed)
+
+# fido = Dog(" ", "Do")
+# print(fido)
+
 # ##################################################################
 
 
+
+
+
+
+
+
+
+
+# #!/usr/bin/env python3
+
+# APPROVED_BREEDS = [
+#     "Mastiff",
+#     "Chihuahua",
+#     "Corgi",
+#     "Shar Pei",
+#     "Beagle",
+#     "French Bulldog",
+#     "Pug",
+#     "Pointer"
+# ]
+
+# # ##################################################################
+# class Dog:
+#     def __init__(self, name="Any", breed="Any"):
+
+#         if isinstance(name, str) and 0 < len(name) <= 25 and len(name) != 0:
+#             self.name = name
+#         else:
+#             print("Name must be string between 1 and 25 characters.")
+
+        
+#         if breed in APPROVED_BREEDS:
+#             self.breed = breed
+#         else:
+#             print("Breed must be in list of approved breeds.")
+
+# fido = Dog(" ", "Do")
+# print(fido)
+
+# # ##################################################################
